01204fa357c6d86797f7ab6bc545f8ffd14bb7de
Sakibul Mowla <sakibulmowla@gmail.com>
2020-08-23 00:41:15 +0100

    Medium, String, Map, Sliding Window

---
+++ solutions/0003. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int startIndex = 0;
+        int ans = 0;
+        unordered_map<char, int> lastIndex;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (lastIndex.find(s[i]) != lastIndex.end()) {
+                startIndex = max(startIndex, lastIndex[s[i]] + 1);
+            }
+            lastIndex[s[i]] = i;
+            ans = max(ans, i - startIndex + 1);
+        }
+        
+        return ans;
+    }
+};
