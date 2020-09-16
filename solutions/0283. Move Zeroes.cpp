dcada1cf08b6163fb596c062658254f9eb56a944
Sakibul Mowla <sakibulmowla@gmail.com>
2020-09-16 20:43:05 +0100

    Easy, Iteration, Linear, Zeros

---
+++ solutions/0283. Move Zeroes.cpp
@@ -0,0 +1,64 @@
+/*
+- Time Complexity - O(n)
+- Space Complexity - O(1)
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0, firstZeroPos = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                while (firstZeroPos != n && nums[firstZeroPos] != 0) {
+                    firstZeroPos++;
+                }
+                if (firstZeroPos != n && firstZeroPos < i) {
+                    swap(nums[i], nums[firstZeroPos]);
+                }
+            }
+        }
+        return;
+    }
+};
+
+--------------------------------------------------------------------------
+/*
+- Time Complexity - O(n)
+- Space Complexity - O(1)
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int l = 0, r = 0, n = nums.size();
+        for ( ; r < n; r++) {
+            if (nums[r] != 0) {
+                nums[l++] = nums[r];
+            }
+        }
+        for ( ; l < n; l++) nums[l] = 0;
+    }
+};
+
+--------------------------------------------------------------------------
+/*
+Input:
+[0,1,0,3,12]
+[0,0,0,1,1]
+[1,1,0,0,0]
+[0,1,0,1,0]
+[1,0,1,0,1]
+[1]
+[0]
+[]
+
+Output:
+[1,3,12,0,0]
+[1,1,0,0,0]
+[1,1,0,0,0]
+[1,1,0,0,0]
+[1,1,1,0,0]
+[1]
+[0]
+[]
+*/
