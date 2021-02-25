7bc5cc73fff901618e9662e23a51ba3e63d7695a
Sakibul Mowla <sakibulmowla@gmail.com>
2021-02-25 01:56:16 +0000

    Hard, DFS, Sorting, Binary Tree, Hashmap, Facebook

--- solutions/0987. Vertical Order Traversal of a Binary Tree.cpp
+++ solutions/0987. Vertical Order Traversal of a Binary Tree.cpp
@@ -1,51 +1,37 @@
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    
-    void traverse(TreeNode* now, int x, int y, map<int, vector<pair<int, int>>>& hash) {
-        if (now != NULL) {
-            if (hash.find(x) == hash.end()) {
-                hash[x] = vector<pair<int, int>>();
-            }
-            hash[x].push_back({y, now->val});
-            traverse(now->left, x - 1, y - 1, hash);
-            traverse(now->right, x + 1, y - 1, hash);
+    void dfs(TreeNode* root, int col, int row, map<int, vector<pair<int, int>>>& store) {
+        if (root == nullptr) {
+            return;
         }
+        store[col].push_back({row, root->val});
+        dfs(root->left, col - 1, row + 1, store);
+        dfs(root->right, col + 1, row + 1, store);
         return;
     }
-    
-    static bool comp(pair<int, int>& a, pair<int, int>& b) {
-        if (a.first == b.first) return a.second < b.second;
-        return a.first > b.first;
-    }
-    
-    vector<int> sortVertical(vector<pair<int, int>>& line) {
-        sort(line.begin(), line.end(), comp);
-        vector<int> res;
-        for (auto val: line) {
-            res.push_back(val.second);
-        }
-        return res;
-    }
-    
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, vector<pair<int, int>>> hash;
-        traverse(root, 0, 0, hash);
-        
+        map<int, vector<pair<int, int>>> store;
+        dfs(root, 0, 0, store);
         vector<vector<int>> ans;
-        for (auto& it: hash) {
-            ans.push_back(sortVertical(it.second));
+        for (auto it: store) {
+            vector<int> tmp;
+            sort(it.second.begin(), it.second.end());
+            for (auto x: it.second) {
+                tmp.push_back(x.second);
+            }
+            ans.push_back(tmp);
         }
-
         return ans;
     }
 };
