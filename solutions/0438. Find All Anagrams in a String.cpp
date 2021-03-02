16a993f8967e45e4c8a6ba3a1f1a9db9b6ddb2d1
Sakibul Mowla <sakibulmowla@gmail.com>
2021-03-02 14:02:13 +0000

    Medium, Sliding Window, Hashing, Facebook

---
+++ solutions/0438. Find All Anagrams in a String.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void update(unordered_map<char, int>& freq, int& cnt, char key, int value) {
+        if (freq.find(key) == freq.end()) {
+            freq[key] = 0;
+        }
+        freq[key] += value;
+        if (value > 0) {
+            if (freq[key] == 0) {
+                cnt--;
+            } else if (freq[key] == 1) {
+                cnt++;
+            }
+        } else {
+            if (freq[key] == 0) {
+                cnt--;
+            } else if (freq[key] == -1) {
+                cnt++;
+            }
+        }
+        return;
+    }
+
+    vector<int> findAnagrams(string s, string p) {
+        unordered_map<char, int> freq;
+    
+        int cnt = 0;
+        for (char ch: p) {
+            update(freq, cnt, ch, 1);
+        }
+
+        vector<int> ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            update(freq, cnt, s[i], -1);
+            if (i >= p.size()) {
+                update(freq, cnt, s[i - p.size()], 1);
+            }
+            if (cnt == 0) {
+                ans.push_back(i - p.size() + 1);
+            }
+        }
+        
+        return ans;
+    }
+};
