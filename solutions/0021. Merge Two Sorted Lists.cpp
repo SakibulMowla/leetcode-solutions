76417d2bf4933a8716287bcd04c857dd390ee9d2
Sakibul Mowla <sakibulmowla@gmail.com>
2021-03-10 20:14:07 +0000

    Easy, Linked List, Merge, Amazon

---
+++ solutions/0021. Merge Two Sorted Lists.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* ans = new ListNode();
+        ListNode* prev = ans;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                prev->next = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        
+        prev->next = l1 == nullptr ? l2 : l1;
+        
+        return ans->next;
+    }
+};
