36487a6bb4c365de7aa3fe83a944dd946bd1b0da
Sakibul Mowla <sakibulmowla@gmail.com>
2021-03-13 22:19:10 +0000

    Easy, BST, Search, Facebook

---
+++ solutions/0270. Closest Binary Search Tree Value.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        int ans = root->val;
+        int tmp = ans;
+        if (target > root->val && root->right) {
+            tmp = closestValue(root->right, target);
+
+        } else if (target < root->val && root->left) {
+            tmp = closestValue(root->left, target); 
+        }
+        if (abs(target - tmp) < abs(target - ans)) {
+            ans = tmp;
+        }
+        return ans;
+    }
+};
