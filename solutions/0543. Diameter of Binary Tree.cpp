f3b3036247846abfe0688daef5f06ed5d2f74e0b
Sakibul Mowla <sakibulmowla@gmail.com>
2021-03-15 22:16:57 +0000

    Easy, Binary Tree, Traverse, Facebook

---
+++ solutions/0543. Diameter of Binary Tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, int& ans) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        int leftPath = dfs(root->left, ans);
+        int rightPath = dfs(root->right, ans);
+        
+        ans = max(ans, leftPath + rightPath);
+        
+        return max(leftPath, rightPath) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans = 0;
+        dfs(root, ans);
+        return ans;
+    }
+};
