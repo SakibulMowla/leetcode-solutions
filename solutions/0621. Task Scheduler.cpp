e00156a9410b26cec7c32e5afc22e0a3a41d970b
Sakibul Mowla <sakibulmowla@gmail.com>
2021-03-16 23:59:43 +0000

    Medium, Priority Queue, Facebook

--- solutions/0621. Task Scheduler.cpp
+++ solutions/0621. Task Scheduler.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
-    
-    int leastInterval(vector<char>& tasks, int n) {
-        map<char, int> freq;
-        for (int i = 0; i < tasks.size(); i++) {
-            if (freq.find(tasks[i]) == freq.end()) {
-                freq[tasks[i]] = 0;
-            }
-            freq[tasks[i]]++;
-        }
-        
-        priority_queue<int> pq;
-        for (auto it: freq) {
-            pq.push(it.second);
+    int leastInterval(vector<char>& tasks, int n)
+    {
+        vector<int> cnt(26, 0);
+        for (char task : tasks) {
+            cnt[task - 'A']++;
         }
 
-        int current = 0;
-        while (!pq.empty()) {
-            int qSize = pq.size();
-            int positions = n + 1;
-            vector<int> tmp;
-            for (int i = 0; i < qSize && positions; i++) {
-                int top = pq.top();
-                pq.pop();
-                if (top > 1) {
-                    tmp.push_back(top - 1);
-                }
-                positions--;
-                current++;
-            }
-            if (tmp.size() && positions) {
-                current += positions;
-            }
-            for (int x: tmp) {
-                pq.push(x);
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i]) {
+                pq.push(cnt[i]);
             }
         }
-        
-        return current;
+
+        int ans = 0;
+        while (!pq.empty()) {
+            int lim = min(n + 1, (int)pq.size());
+            vector<int> tmp;
+            for (int i = 0; i < lim; i++) {
+                int top = pq.top();
+                pq.pop();
+                if (top - 1) {
+                    tmp.push_back(top - 1);
+                }
+            }
+            ans += lim;
+            for (int num : tmp) {
+                pq.push(num);
+            }
+            if (pq.size()) {
+                ans += n + 1 - lim;
+            }
+        }
+
+        return ans;
     }
 };
