81957810e1e0211743fe63689a59385739c37da3
Sakibul Mowla <sakibulmowla@gmail.com>
2021-03-19 16:17:03 +0000

    Medium, Stack, Parenthesis, Facebook

---
+++ solutions/1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        string ans;
+        int n = s.size();
+        
+        vector<int> rightCount(n + 1, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            rightCount[i] = rightCount[i + 1] + (int)(s[i] == ')');
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            if (isalpha(s[i])) {
+                ans += s[i];
+            } else {
+                if (s[i] == '(') {
+                    sum++;
+                    ans += s[i];
+                } else {
+                    if (sum) {
+                        ans += s[i];
+                        sum--;
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < n && sum; i++) {
+            if (s[i] == '(') {
+                ans.erase()
+            }
+        }
+        
+        return ans;
+    }
+};
