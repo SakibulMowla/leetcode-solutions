5b8195565a9be786a010507d41cfb67c4b4e2d47
Sakibul Mowla <sakibulmowla@gmail.com>
2021-04-29 04:54:34 +0100

    Easy, String, Iteration, Hashing, Facebook

--- solutions/0242. Valid Anagram.cpp
+++ solutions/0242. Valid Anagram.cpp
@@ -21,3 +21,29 @@ public:
         return true;
     }
 };
+
+// ==============================================
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) {
+            return false;
+        }
+        
+        vector<int> freq(26, 0);
+        
+        for (char ch: s) {
+            freq[ch - 'a']++;
+        }
+        
+        for (char ch: t) {
+            freq[ch - 'a']--;
+            if (freq[ch - 'a'] < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
