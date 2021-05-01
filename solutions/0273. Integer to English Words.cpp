b6a60d6039b206c188b6f5490f7c17dbd0e8c8fe
Sakibul Mowla <sakibulmowla@gmail.com>
2021-05-01 21:29:38 +0100

    Hard, String, Simulation, Facebook

--- solutions/0273. Integer to English Words.cpp
+++ solutions/0273. Integer to English Words.cpp
@@ -1,89 +1,79 @@
 class Solution {
-public:
-    
+private:
     unordered_map<int, string> numToWord = {
-        {0, "Zero"},
-        {1, "One"},
-        {2, "Two"},
-        {3, "Three"},
-        {4, "Four"},
-        {5, "Five"},
-        {6, "Six"},
-        {7, "Seven"},
-        {8, "Eight"},
-        {9, "Nine"},
-        {10, "Ten"},
-        {11, "Eleven"},
-        {12, "Twelve"},
-        {13, "Thirteen"},
-        {14, "Fourteen"},
-        {15, "Fifteen"},
-        {16, "Sixteen"},
-        {17, "Seventeen"},
-        {18, "Eighteen"},
-        {19, "Nineteen"},
-        {20, "Twenty"},
-        {30, "Thirty"},
-        {40, "Forty"},
-        {50, "Fifty"},
-        {60, "Sixty"},
-        {70, "Seventy"},
-        {80, "Eighty"},
-        {90, "Ninety"}
+        { 0, "Zero" },
+        { 1, "One" },
+        { 2, "Two" },
+        { 3, "Three" },
+        { 4, "Four" },
+        { 5, "Five" },
+        { 6, "Six" },
+        { 7, "Seven" },
+        { 8, "Eight" },
+        { 9, "Nine" },
+        { 10, "Ten" },
+        { 11, "Eleven" },
+        { 12, "Twelve" },
+        { 13, "Thirteen" },
+        { 14, "Fourteen" },
+        { 15, "Fifteen" },
+        { 16, "Sixteen" },
+        { 17, "Seventeen" },
+        { 18, "Eighteen" },
+        { 19, "Nineteen" },
+        { 20, "Twenty" },
+        { 30, "Thirty" },
+        { 40, "Forty" },
+        { 50, "Fifty" },
+        { 60, "Sixty" },
+        { 70, "Seventy" },
+        { 80, "Eighty" },
+        { 90, "Ninety" }
     };
     
-    const int thousand = 1000;
-    const int million = thousand * thousand;
-    const int billion = thousand * million;
-    
-    void threeDigit(int num, vector<string>& ret) {
-        if (num > 99) {
-            ret.push_back(numToWord[num / 100]);
-            ret.push_back("Hundred");
+    void divideAndConquer(int num, vector<string>& ans) {
+        if (num >= 1E9) {
+            divideAndConquer(num / 1E9, ans);
+            ans.push_back("Billion");
+            num %= (int)1E9;
         }
-        num %= 100;
-        if (num > 0 && num <= 20) {
-            ret.push_back(numToWord[num]);
-        } else if (num > 20) {
-            ret.push_back(numToWord[(num / 10) * 10]);
-            if (num % 10) {
-                ret.push_back(numToWord[num % 10]);
-            }
+        if (num >= 1E6) {
+            divideAndConquer(num / 1E6, ans);
+            ans.push_back("Million");
+            num %= (int)1E6;
+        }
+        if (num >= 1E3) {
+            divideAndConquer(num / 1E3, ans);
+            ans.push_back("Thousand");
+            num %= (int)1E3;
+        }
+        if (num >= 100) {
+            divideAndConquer(num / 100, ans);
+            ans.push_back("Hundred");
+            num %= 100;
+        }
+        if (num >= 20) {
+            ans.push_back(numToWord[num / 10 * 10]);
+            num %= 10;
+        }
+        if (num) {
+            ans.push_back(numToWord[num]);
         }
         return;
     }
 
-    string numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-    
-        vector<string> ans;
-        if (num / billion) {
-            threeDigit(num / billion, ans);
-            ans.push_back("Billion");
-            num %= billion;
-        }
-        if (num / million) {
-            threeDigit(num / million, ans);
-            ans.push_back("Million");
-            num %= million;
-        }
-        if (num / thousand) {
-            threeDigit(num / thousand, ans);
-            ans.push_back("Thousand");
-            num %= thousand;
-        }
-        if (num) {
-            threeDigit(num, ans);
-        }
-        
-        string words;
+    string join(vector<string> ans, string del) {
+        string sentence;
         for (int i = 0; i < ans.size(); i++) {
-            if (i) words += " ";
-            words += ans[i];
+            sentence = sentence + (i ? del : "") + ans[i];
         }
-        
-        return words;
+        return sentence;
+    }
+    
+public:
+    string numberToWords(int num) {
+        vector<string> ans;
+        divideAndConquer(num, ans);
+        return num ? join(ans, " ") : numToWord[0];
     }
 };
