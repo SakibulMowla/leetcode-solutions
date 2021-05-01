ef8ab18c2a64fe4ebf371e8e1eb9cf2f1b5f9e5b
Sakibul Mowla <sakibulmowla@gmail.com>
2021-05-01 05:13:24 +0100

    Medium, BFS, Binary Tree, Traversal, Facebook

---
+++ solutions/0102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        queue<pair<TreeNode*, int>> q;
+        if (root) q.push({root, 0});
+        
+        vector<vector<int>> ans;
+        int curLevel = -1;
+        
+        while (!q.empty()) {
+            auto front = q.front();
+            q.pop();
+            
+            TreeNode* cur = front.first;
+            int level = front.second;
+            
+            if (level > curLevel) {
+                curLevel = level;
+                ans.push_back(vector<int> ());
+            }
+            
+            ans[curLevel].push_back(cur->val);
+            
+            if (cur->left) {
+                q.push({cur->left, level + 1});
+            }
+            
+            if (cur->right) {
+                q.push({cur->right, level + 1});
+            }
+        }
+        
+        return ans;
+    }
+};
