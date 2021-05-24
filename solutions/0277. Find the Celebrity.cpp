275697f683024017d395ce6819665765b4e2c2e0
Sakibul Mowla <sakibulmowla@gmail.com>
2021-05-24 22:35:37 +0100

    Medium, Deduction, Facebook

---
+++ solutions/0277. Find the Celebrity.cpp
@@ -0,0 +1,32 @@
+// ==============================================
+// O(2 * n)
+// ==============================================
+
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+private:
+    int isCelebrity(int candidate, int n) {
+        for (int i = 0; i < n; i++) {
+            if (i == candidate) {
+                continue;
+            }
+            if (!knows(i, candidate) || knows(candidate, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+public:
+    int findCelebrity(int n) {
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(candidate, i)) {
+                candidate = i;
+            }
+        }
+        return isCelebrity(candidate, n) ? candidate : -1;
+    }
+};
