65c24e03410343f28142e0dafb0fc048659d516c
Sakibul Mowla <sakibulmowla@gmail.com>
2021-05-28 01:23:24 +0100

    Easy, Palindrome, String, Facebook

--- solutions/0680. Valid Palindrome II.cpp
+++ solutions/0680. Valid Palindrome II.cpp
@@ -1,3 +1,7 @@
+// =======================================================
+// Time - O(n) Memory - O(n)
+// =======================================================
+
 class Solution {
 public:
     bool isPalindrome(string& s, vector<int>& positions) {
@@ -26,5 +30,44 @@ public:
         }
 
         return flag;
+    }
+};
+
+// =======================================================
+// Time - O(n) Memory - O(1)
+// =======================================================
+
+class Solution {
+private:
+    bool isPalindrome(string& s, int skipIndex) {
+        int l = 0;
+        int r = s.size() - 1;
+        
+        while (l < r) {
+            if (l == skipIndex) {
+                l++;
+            } else if (r == skipIndex) {
+                r--;
+            } else {
+                if (s[l] != s[r]) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+        }
+        
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int n = s.size();
+        for (int i = 0; i < n / 2; i++) {
+            if (s[i] != s[n - i - 1]) {
+                return isPalindrome(s, i) || isPalindrome(s, n - i - 1);
+            }
+        }
+        return true;
     }
 };
