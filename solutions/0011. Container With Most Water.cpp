480e9634efbaa9e0cee1101c9826b218d541324d
Sakibul Mowla <sakibulmowla@gmail.com>
2021-05-31 16:01:07 +0100

    Medium, Two Pointer, Google

---
+++ solutions/0011. Container With Most Water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int l = 0;
+        int r = height.size() - 1;
+        while (l < r) {
+            ans = max(ans, min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        
+        return ans;
+    }
+};
