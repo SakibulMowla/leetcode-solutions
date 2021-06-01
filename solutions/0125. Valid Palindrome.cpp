3b175c34e55a46279d4fdaf547fef183068aacc0
Sakibul Mowla <sakibulmowla@gmail.com>
2021-06-01 17:59:26 +0100

    Easy, Palindrome, Facebook

--- solutions/0125. Valid Palindrome.cpp
+++ solutions/0125. Valid Palindrome.cpp
@@ -1,22 +1,26 @@
 class Solution {
-public:
+private:
+    bool isValidChar(char ch) {
+        return isalpha(ch) || isdigit(ch);
+    }
 
+public:
     bool isPalindrome(string s) {
         int l = 0;
         int r = s.size() - 1;
         
         while (l < r) {
-            while (l < r && !(isalpha(s[l]) || isdigit(s[l]))) {
+            if (!isValidChar(s[l])) {
                 l++;
-            }
-            while (l < r && !(isalpha(s[r]) || isdigit(s[r]))) {
+            } else if (!isValidChar(s[r])) {
+                r--;
+            } else {
+                if (tolower(s[l]) != tolower(s[r])) {
+                    return false;
+                }
+                l++;
                 r--;
             }
-            if (tolower(s[l]) != tolower(s[r])) {
-                return false;
-            }
-            l++;
-            r--;
         }
         
         return true;
