abd71d824a363035663befae287fe2a23c8fda26
Sakibul Mowla <sakibulmowla@gmail.com>
2021-06-03 16:57:08 +0100

    Easy, String, Google

---
+++ solutions/0014. Longest Common Prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+
+        for (int i = 0; i < strs[0].size(); i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j].size() == i || strs[j][i] != strs[0][i]) {
+                    return strs[0].substr(0, i);
+                }
+            }
+        }
+        
+        return strs[0];
+    }
+};
