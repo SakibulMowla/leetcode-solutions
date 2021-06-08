6f24382876c347a8b4fdf86f5ba31390069fd8dc
Sakibul Mowla <sakibulmowla@gmail.com>
2021-06-08 22:16:15 +0100

    Easy, Hashing, Google

--- solutions/0359. Logger Rate Limiter.cpp
+++ solutions/0359. Logger Rate Limiter.cpp
@@ -1,20 +1,19 @@
 class Logger {
+private:
+unordered_map<string, int> messageToTime;
+
 public:
     /** Initialize your data structure here. */
-    
-    unordered_map<string, int> hash;
-
     Logger() {
-        hash.clear();        
+        messageToTime.clear();
     }
     
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity. */
     bool shouldPrintMessage(int timestamp, string message) {
-        auto it = hash.find(message);
-        if (it == hash.end() || timestamp - (*it).second >= 10) {
-            hash[message] = timestamp;
+        if (messageToTime.find(message) == messageToTime.end() || timestamp - messageToTime[message] >= 10) {
+            messageToTime[message] = timestamp;
             return true;
         }
         return false;
