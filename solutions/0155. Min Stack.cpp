4f12f695f694889bb41a88ed30ea43642e9406a6
Sakibul Mowla <sakibulmowla@gmail.com>
2021-06-15 17:05:42 +0100

    Easy, Stack, Data Structure

--- solutions/0155. Min Stack.cpp
+++ solutions/0155. Min Stack.cpp
@@ -44,3 +44,60 @@ public:
  * int param_4 = obj->getMin();
  */
  
+// ================================================================
+
+class MinStack {
+private:
+    stack<int> stk;
+    stack<pair<int, int>> minStk;
+
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+                
+    }
+    
+    void push(int val) {
+        stk.push(val);
+        if (minStk.empty() || val < minStk.top().first) {
+            minStk.push({ val, 1 });
+        } else if (val == minStk.top().first) {
+            pair<int, int> minStkTop = minStk.top();
+            minStk.pop();
+            minStkTop.second++;
+            minStk.push(minStkTop);
+        }
+        
+        return;
+    }
+    
+    void pop() {
+        if (minStk.top().first == stk.top()) {
+            pair<int, int> minStkTop = minStk.top();
+            minStk.pop();
+            minStkTop.second--;
+            if (minStkTop.second != 0) {
+                minStk.push(minStkTop);   
+            }
+        }
+        stk.pop();
+        return;
+    }
+    
+    int top() {
+        return stk.top();
+    }
+    
+    int getMin() {
+        return minStk.top().first;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
