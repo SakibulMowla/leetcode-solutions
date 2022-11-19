87ae9354ea4f50279d12718bd7babe1d80646de3
Sakibul Mowla <sakibulmowla@gmail.com>
2022-11-19 19:50:04 +0000

    Easy, Linked List, Merge, Amazon, Grind75

--- solutions/0021. Merge Two Sorted Lists.cpp
+++ solutions/0021. Merge Two Sorted Lists.cpp
@@ -30,3 +30,22 @@ public:
         return ans->next;
     }
 };
+----------------------------------------------------------------
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (!list1 && !list2) {
+            return nullptr;
+        } else if (!list1) {
+            return list2;
+        } else if (!list2) {
+            return list1;
+        } else if (list1->val < list2->val) {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        } else {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+    }
+};
