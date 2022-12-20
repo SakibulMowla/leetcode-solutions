036789718febc1141b37abb7dae6fe7a75a444aa
Sakibul Mowla <sakibulmowla@gmail.com>
2022-12-20 20:37:08 +0000

    Easy, Recursion, Binary Tree, Pointer, Amazon, Google, Microsoft

--- solutions/0226. Invert Binary Tree.cpp
+++ solutions/0226. Invert Binary Tree.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// Recursive
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
@@ -18,6 +20,31 @@ public:
         invertTree(root->left);
         invertTree(root->right);
         swap(root->left, root->right);
+        return root;
+    }
+};
+
+// Iterative
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) {
+            return root;
+        }
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        while (!nodes.empty()) {
+            TreeNode* current = nodes.front();
+            nodes.pop();
+            swap(current->left, current->right);
+            if (current->left) {
+                nodes.push(current->left);
+            }
+            if (current->right) {
+                nodes.push(current->right);
+            }
+        }
+
         return root;
     }
 };
