641b5eeccd5b27ce58a1868aaa052bc7d8f04675
Sakibul Mowla <sakibulmowla@gmail.com>
2022-12-25 16:46:58 +0000

    Medium, Interval Merge, Facebook

--- solutions/0057. Insert Interval.cpp
+++ solutions/0057. Insert Interval.cpp
@@ -2,23 +2,18 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> ans;
-        
-        int n = intervals.size();
-        
-        vector<int> current = newInterval;
-        for (int i = 0; i < n; i++) {
-            if (intervals[i][1] < current[0]) {
+        for (int i = 0; i < intervals.size(); i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                ans.push_back(newInterval);
+                newInterval = intervals[i];
+            } else if (intervals[i][1] < newInterval[0]) {
                 ans.push_back(intervals[i]);
-            } else if (intervals[i][0] > current[1]) {
-                ans.push_back(current);
-                current = intervals[i];
             } else {
-                current[0] = min(current[0], intervals[i][0]);
-                current[1] = max(current[1], intervals[i][1]);
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
+                newInterval[1] = max(newInterval[1], intervals[i][1]);
             }
         }
-        ans.push_back(current);
-        
+        ans.push_back(newInterval);
         return ans;
     }
 };
