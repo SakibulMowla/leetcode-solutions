2113103a7902a68eeb5c8d9b42d64a0a8a79c248
Sakibul Mowla <sakibulmowla@gmail.com>
2022-12-31 13:50:19 +0000

    Easy, Palindrome, Amazon, Apple, Grinf75

---
+++ solutions/0409. Longest Palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> charToCount;
+        for (char ch: s) {
+            charToCount[ch]++;
+        }
+
+        bool oddPresent = false;
+        int length = 0;
+        for (auto it: charToCount) {
+            int count = it.second;
+            if (count % 2 == 1) {
+                oddPresent = true;
+                count--;
+            }
+            length += count;
+        }
+
+        return length + (int)oddPresent;
+    }
+};
