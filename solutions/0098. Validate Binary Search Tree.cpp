4c63da6cc80483f055c3c1660af2d08300bfcdd0
Sakibul Mowla <sakibulmowla@gmail.com>
2023-01-01 17:36:22 +0000

    Medium, BST, Facebook, Grind75

--- solutions/0098. Validate Binary Search Tree.cpp
+++ solutions/0098. Validate Binary Search Tree.cpp
@@ -8,7 +8,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ */ 
 class Solution {
 public:
     bool isValidBST(TreeNode* root, TreeNode* minimum = nullptr, TreeNode* maximum = nullptr) {
