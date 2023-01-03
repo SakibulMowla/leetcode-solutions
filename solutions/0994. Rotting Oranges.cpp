accd5da09ff7993458f6c24e61e9a6950899bf1f
Sakibul Mowla <sakibulmowla@gmail.com>
2023-01-03 22:41:15 +0000

    Medium, BFS, Amazon, Grind75

--- solutions/0994. Rotting Oranges.cpp
+++ solutions/0994. Rotting Oranges.cpp
@@ -1,50 +1,45 @@
 class Solution {
+private:
+    vector<int> dx = {-1, 0, 0, 1};
+    vector<int> dy = {0, -1, 1, 0};
+    
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         int rows = grid.size();
-        if (rows == 0) {
-            return 0;
-        }
         int cols = grid[0].size();
-        
-        int rottenCount = 0;
-        int totalOrange = 0;
-        
-        queue<pair<int, int>> q;
+
+        queue<pair<int, int>> rottens;
+        int fresh = 0;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 2) {
-                    rottenCount++;
-                    q.push({i, j});
+                    rottens.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
                 }
-                totalOrange += (grid[i][j] == 1 || grid[i][j] == 2);
             }
         }
-        
-        int ans = 0;
-        vector<vector<int>> timeCount(rows, vector<int>(cols, 0));
-        
-        vector<int> dx{-1, 1, 0, 0};
-        vector<int> dy{0, 0, -1, 1};
 
-        while (!q.empty()) {
-            pair<int, int> front = q.front();
-            q.pop();
-            
-            pair<int, int> step;
+        int ans = 0;
+        while (!rottens.empty()) {
+            int x = rottens.front().first;
+            int y = rottens.front().second;
+            rottens.pop();
             for (int k = 0; k < 4; k++) {
-                step.first = front.first + dx[k];
-                step.second = front.second + dy[k];
-                if (step.first >= 0 && step.first < rows && step.second >= 0 && step.second < cols && grid[step.first][step.second] == 1 && timeCount[step.first][step.second] == 0) {
-                    timeCount[step.first][step.second] = timeCount[front.first][front.second] + 1;
-                    ans = max(ans, timeCount[step.first][step.second]);
-                    q.push(step);
-                    rottenCount++;
-                }
+                int newX = x + dx[k];
+                int newY = y + dy[k];
+                if (newX >= 0 && newX < rows &&
+                    newY >= 0 && newY < cols &&
+                    grid[newX][newY] == 1) {
+                        grid[newX][newY] = grid[x][y] + 1;
+                        rottens.push({newX, newY});
+                        fresh--;
+                        ans = max(ans, grid[newX][newY] - 2);
+                    }
             }
         }
-        
-        return rottenCount == totalOrange ? ans : -1;
+
+        return fresh == 0 ? ans : -1;
     }
 };
