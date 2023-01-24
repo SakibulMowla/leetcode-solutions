0d19f71ae1dcd26e54fe695f9b37125c4273c6d1
Sakibul Mowla <sakibulmowla@gmail.com>
2023-01-24 21:29:47 +0000

    Medium, DP, Amazon, Grind75

--- solutions/1155. Number of Dice Rolls With Target Sum.cpp
+++ solutions/1155. Number of Dice Rolls With Target Sum.cpp
@@ -1,3 +1,4 @@
+// Recursive DP
 class Solution {
 public:
     const int mod = 1E9 + 7;
@@ -22,5 +23,29 @@ public:
     int numRollsToTarget(int d, int f, int target) {
         vector<vector<int>> dp(d + 1, vector<int> (target + 1, -1));
         return solve(d, f, target, dp);
+    }
+};
+
+// Iterative DP
+class Solution {
+private: 
+    const int mod = 1E9 + 7;
+public:
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n + 1, vector<int> (target + 1, 0));
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int face = 1; face <= k; face++) {
+                for (int sum = face; sum <= target; sum++) {
+                    dp[i][sum] = dp[i][sum] + dp[i - 1][sum - face];
+                    if (dp[i][sum] >= mod) {
+                        dp[i][sum] -= mod;
+                    }
+                }
+            }
+        }
+
+        return dp[n][target];
     }
 };
