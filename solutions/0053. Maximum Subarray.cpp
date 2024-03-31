d7750afaeda9f4c32122f3ee2b54b5f114898aff
Sakibul Mowla <sakibulmowla@gmail.com>
2024-04-01 00:45:25 +0100

    Easy, Sum, Kadane's Algorithm, Google, Grind75

--- solutions/0053. Maximum Subarray.cpp
+++ solutions/0053. Maximum Subarray.cpp
@@ -1,3 +1,7 @@
+/*
+Time - O(n)
+Space - O(1)
+*/
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -9,5 +13,45 @@ public:
             maxSum = max(maxSum, currentSum);
         }
         return maxSum;
+    }
+};
+
+// ---------------------------------------
+/*
+Time - O(nlogn)
+Space - O(logn)
+*/
+
+class Solution {
+private:
+    int maxSubArrayRange(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return numeric_limits<int>::min();
+        }
+
+        int mid = (left + right) / 2;
+        int maxLeftSum = 0, maxRightSum = 0;
+
+        for (int i = mid + 1, sum = 0; i <= right; i++) {
+            sum += nums[i];
+            maxRightSum = max(maxRightSum, sum);
+        }
+
+        for (int i = mid - 1, sum = 0; i >= left; i--) {
+            sum += nums[i];
+            maxLeftSum = max(maxLeftSum, sum);
+        }
+
+        int bestCombined = nums[mid] + maxLeftSum + maxRightSum;
+
+        int leftAns = maxSubArrayRange(nums, left, mid - 1);
+        int rightAns = maxSubArrayRange(nums, mid + 1, right);
+
+        return max(bestCombined, max(leftAns, rightAns));
+    }
+
+public:
+    int maxSubArray(vector<int>& nums) {
+        return maxSubArrayRange(nums, 0, nums.size() - 1);
     }
 };
