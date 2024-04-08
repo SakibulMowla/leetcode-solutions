9f40eba88cff9b13d5f3ec568377ef54d282ad4b
Sakibul Mowla <sakibulmowla@gmail.com>
2024-04-08 10:15:18 +0100

    Medium, Tree, LCA, DFS, Facebook, Grind75

--- solutions/0236. Lowest Common Ancestor of a Binary Tree.cpp
+++ solutions/0236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -49,3 +49,41 @@ public:
         return par[p];
     }
 };
+
+// --------------------------------------------------
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* ans;
+    
+    bool traverse(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) {
+            return false;
+        }
+
+        int leftCount = traverse(root->left, p, q) ? 1 : 0;
+        int rightCount = traverse(root->right, p, q) ? 1 : 0;
+        int midCount = (root == p || root == q) ? 1 : 0;
+
+        if (leftCount + midCount + rightCount == 2) {
+            ans = root;
+        }
+
+        return leftCount + midCount + rightCount > 0;
+    }
+
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        traverse(root, p, q);
+        return ans;
+    }
+};
