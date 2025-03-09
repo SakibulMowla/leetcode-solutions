fd72ce854640ce91f849d95195f6582527835a7a
Sakibul Mowla <sakibulmowla@gmail.com>
2025-03-09 16:55:08 +0000

    Medium, Parsing, Stoi, Facebook, Grind75

--- solutions/0008. String to Integer (atoi).cpp
+++ solutions/0008. String to Integer (atoi).cpp
@@ -1,5 +1,58 @@
 class Solution {
 private:
+    void truncateWhitespaces(string& s) {
+        int index = 0;
+        while (index < s.size() && s[index] == ' ') {
+            index++;
+        }
+        s = s.substr(index);
+        return;
+    }
+
+    int sign(string& s) {
+        if (s.size() == 0) {
+            return 1;
+        }
+
+        int mul = s[0] == '-' ? -1 : 1;
+        if (s[0] == '+' || s[0] == '-') {
+            s = s.substr(1);
+        }
+
+        return mul;
+    }
+
+    long long conversion(string& s) {
+        long long num = 0;
+        for (char ch: s) {
+            if (!isdigit(ch) || num >= numeric_limits<int>::max()) {
+                break;
+            }
+            num = num * 10 + (ch - '0');
+        }
+        return num;
+    }
+
+    int rounding(long long num, int mul) {
+        num *= mul;
+        num = max(num, (long long)numeric_limits<int>::min());
+        num = min(num, (long long)numeric_limits<int>::max());
+        return (int)num;
+    }
+
+public:
+    int myAtoi(string s) {
+        truncateWhitespaces(s);
+        int mul = sign(s);
+        long long num = conversion(s);
+        return rounding(num, mul);
+    }
+};
+
+//--------------------------------------
+
+class Solution {
+private:
     void removeLeadingZeros(string& s) {
         while (s.size() && s[0] == ' ') {
             s.erase(0, 1);
