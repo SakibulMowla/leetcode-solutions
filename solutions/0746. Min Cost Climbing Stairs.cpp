df12563b3dad102388241d8e648bddd35de02b85
Sakibul Mowla <sakibulmowla@gmail.com>
2025-04-16 12:23:57 +0100

    Easy, Array, DP

---
+++ solutions/0746. Min Cost Climbing Stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        
+        vector<int> dp(n + 1, numeric_limits<int>::max());
+        dp[0] = dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = min(dp[i - 1] + cost[i - 1],
+                        dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+    }
+};
