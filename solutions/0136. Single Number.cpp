19424824bcb29e10994558c8ca0f1f6a11858a6b
Sakibul Mowla <sakibulmowla@gmail.com>
2025-04-17 08:25:01 +0100

    Easy, Bit Manipulation

--- solutions/0136. Single Number.cpp
+++ solutions/0136. Single Number.cpp
@@ -20,3 +20,16 @@ public:
         return ans;
     }
 };
+
+// ---------------------------------------------
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int num: nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+};
