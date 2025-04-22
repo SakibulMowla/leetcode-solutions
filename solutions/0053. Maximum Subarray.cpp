37cc4c524ca11f6b5a6ca5bcbd216dca674683a0
Sakibul Mowla <sakibulmowla@gmail.com>
2025-04-22 12:36:39 +0100

    Easy, Sum, Kadane's Algorithm, Grind75

--- solutions/0053. Maximum Subarray.cpp
+++ solutions/0053. Maximum Subarray.cpp
@@ -5,14 +5,15 @@ Space - O(1)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int maxSum = nums[0];
-        int currentSum = 0;
-        for (int num: nums) {
-            currentSum += num;
-            currentSum = max(num, currentSum);
-            maxSum = max(maxSum, currentSum);
+        int ans = nums[0];
+        for (int i = 0, sum = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            ans = max(ans, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
         }
-        return maxSum;
+        return ans;
     }
 };
 
@@ -53,5 +54,48 @@ private:
 public:
     int maxSubArray(vector<int>& nums) {
         return maxSubArrayRange(nums, 0, nums.size() - 1);
+    }
+};
+
+// 
+/*
+Optimized Divide and Conquer
+Time - O(nlogn)
+Space - O(n)
+*/
+
+class Solution {
+private:
+    int divideAndConquer(int left, int right, vector<int>& nums, vector<int>& prefix, vector<int>& suffix) {
+        if (left == right) {
+            return nums[left];
+        }
+        
+        int mid = left + (right - left) / 2;
+        
+        int best = max(
+                       prefix[mid] + suffix[mid + 1],
+                       max(
+                           divideAndConquer(left, mid, nums, prefix, suffix),
+                           divideAndConquer(mid + 1, right, nums, prefix, suffix)
+                       )
+                   );
+        
+        return best;
+    }
+
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> prefix(nums), suffix(nums);
+        
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            prefix[i] += max(0, prefix[i - 1]);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] += max(0, suffix[i + 1]);
+        }
+
+        return divideAndConquer(0, n - 1, nums, prefix, suffix);
     }
 };
