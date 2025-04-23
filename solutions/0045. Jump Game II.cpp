be97116e9fb70423dff2aa3a61fefc763aec94e4
Sakibul Mowla <sakibulmowla@gmail.com>
2025-04-23 08:01:45 +0100

    Medium, Array, Greedy

--- solutions/0045. Jump Game II.cpp
+++ solutions/0045. Jump Game II.cpp
@@ -1,3 +1,28 @@
+// Time- O(n * 1000)
+// Memory - O(n)
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, n);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                dp[i + j] = min(dp[i + j], 1 + dp[i]);
+            }
+        }
+
+        return dp[n - 1];
+    }
+};
+
+// -------------------------------------------------
+
+// Time- O(n)
+// Memory - O(1)
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
