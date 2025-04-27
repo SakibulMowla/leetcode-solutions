9aa1de42845ab6bdecfe55badb7274f4ec244d9b
Sakibul Mowla <sakibulmowla@gmail.com>
2025-04-27 11:40:06 +0100

    Medium, Array, Binary Search, DP

--- solutions/0300. Longest Increasing Subsequence.cpp
+++ solutions/0300. Longest Increasing Subsequence.cpp
@@ -1,29 +1,56 @@
+// Time - O(n ^ 2)
+// Memory  - O(n)
+
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> lis(n + 1, numeric_limits<int>::max());
+        vector<int> dp(n, 0);
+        int ans = 0;
         
-        int lisLen = 0;
         for (int i = 0; i < n; i++) {
-            int lo = 0;
-            int hi = lisLen;
-            int index;
-            
-            while (lo <= hi) {
-                int mid = lo + (hi - lo) / 2;
-                if (nums[i] <= lis[mid]) {
-                    index = mid;
-                    hi = mid - 1;
-                } else {
-                    lo = mid + 1;
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], 1 + dp[j]);
                 }
             }
-            
-            lis[index] = nums[i];
-            lisLen = max(lisLen, index + 1);
+            ans = max(ans, dp[i]);
         }
-        
-        return lisLen;
+
+        return ans;
+    }
+};
+
+// ------------------------------------------
+
+// Time - O(nlog(n))
+// Memory  - O(n)
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> lis(n);
+        int len = 0;
+
+        for (int i = 0; i < n; i++) {
+            int low = 0, high = len - 1, index = len;    
+            
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (nums[i] <= lis[mid]) {
+                    index = mid;
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+
+            lis[index] = nums[i];
+            len = max(len, index + 1);
+        }
+
+        return len;
     }
 };
