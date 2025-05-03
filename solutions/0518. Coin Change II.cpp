fccf260c14400c337f0548f92a3b076a38ec51d0
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-03 11:39:03 +0100

    Medium, Array, DP

---
+++ solutions/0518. Coin Change II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<uint> dp(amount + 1, 0);
+        dp[0] = 1;
+        
+        for (int coin: coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+};
