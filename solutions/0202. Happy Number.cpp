f037e56cc9c8b4bbfcba8b26c9a76a7cf7a5cd25
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-04 10:14:46 +0100

    Easy, Math, Implementation

--- solutions/0202. Happy Number.cpp
+++ solutions/0202. Happy Number.cpp
@@ -1,28 +1,59 @@
+// Time - O(log n)
+// Memory - O(1)
+
 class Solution {
-public:
-    
-    int calc(int n) {
-        int ret = 0;
+private:
+    int squareSum(int n) {
+        int sum = 0;
         while (n) {
             int digit = n % 10;
-            ret += digit * digit;
+            sum += digit * digit;
             n /= 10;
         }
-        return ret;
+        return sum;
     }
-
+    
+public:
     bool isHappy(int n) {
-        map<int, bool> found;
-        while(true) {
+        unordered_set<int> seen;
+        
+        while (seen.find(n) == seen.end()) {
             if (n == 1) {
                 return true;
             }
-            if (found.find(n) != found.end()) {
-                break;
-            }
-            found[n] = true;
-            n = calc(n);
+            seen.insert(n);
+            n = squareSum(n);
         }
+
         return false;
+    }
+};
+
+// ------------------------------------------------------
+
+// Time - O(log n)
+// Memory - O(1)
+
+class Solution {
+private:
+    int squareSum(int n) {
+        int sum = 0;
+        while (n) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+public:
+    bool isHappy(int n) {
+        int slow = n, fast = squareSum(n);
+        while (slow != fast) {
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+        }
+
+        return fast == 1;
     }
 };
