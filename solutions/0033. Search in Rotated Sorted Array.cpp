f11eac01550d713e47cb90fd694aef1d74149fc6
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-04 06:40:17 +0100

    Medium, Binary Search, Grind75

--- solutions/0033. Search in Rotated Sorted Array.cpp
+++ solutions/0033. Search in Rotated Sorted Array.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0, high = n - 1;
+        int low = 0, high = nums.size() - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[mid] >= nums[low]) {
-                if (target >= nums[low] && target < nums[mid]) {
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
             } else {
-                if (target <= nums[high] && target > nums[mid]) {
+                if (nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
                 }
             }
         }
+
         return -1;
     }
 };
