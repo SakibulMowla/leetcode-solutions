79edb1aa1a9bffc31bea52522749ee60b958b383
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-22 21:46:53 +0100

    Easy, Array, Greedy

---
+++ solutions/0605. Can Place Flowers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        flowerbed.insert(flowerbed.begin() + 0, 0);
+        flowerbed.push_back(0);
+
+        for (int i = 1; i < flowerbed.size() - 1 && n > 0; i++) {
+            if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+
+        return n == 0;
+    }
+};
