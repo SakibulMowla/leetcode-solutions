51f5fb3931dd70a2b16abbe3cb6a34de0a52f30a
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-27 10:57:39 +0100

    Hard, DFS, Component, Matrix

--- solutions/0827. Making A Large Island.cpp
+++ solutions/0827. Making A Large Island.cpp
@@ -1,77 +1,77 @@
 class Solution {
 private:
-    int rows;
-    int cols;
+    int n, component;
+    vector<int> dx = {-1, 1, 0, 0};
+    vector<int> dy = {0, 0, -1, 1};
+    vector<vector<int>> gridToComponent;
+    unordered_map<int, int> componentToSize;
 
-    vector<int> dx = { -1, 1, 0, 0 };
-    vector<int> dy = { 0, 0, -1, 1 };
-
-    bool check(int x, int y, vector<vector<int> >& grid, vector<vector<bool> >& visited) {
-        return x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 1 && !visited[x][y];
+    bool isWithinGrid(int newX, int newY) {
+        return newX >= 0 && newX < n && newY >= 0 && newY < n;
     }
 
-    bool check(int x, int y, vector<vector<int> >& grid) {
-        return x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 1;
+    void traverse(int x, int y, vector<vector<int>>& grid, int& componentSize) {
+        gridToComponent[x][y] = component;
+        componentSize++;
+        for (int k = 0; k < 4; k++) {
+            int newX = x + dx[k];
+            int newY = y + dy[k];
+            if (isWithinGrid(newX, newY) && 
+                grid[newX][newY] == 1 &&
+                gridToComponent[newX][newY] == 0) {
+                traverse(newX, newY, grid, componentSize);
+            }
+        }
+        return;
     }
 
-    int dfs(int x, int y, vector<vector<int> >& grid, vector<vector<bool> >& visited, vector<vector<int> >& compId, int curId) {
-        int ret = 1;
-        visited[x][y] = true;
-        compId[x][y] = curId;
+    int findBestAnswer(int x, int y, vector<vector<int>>& grid) {
+        if (grid[x][y] == 1) {
+            int component = gridToComponent[x][y];
+            return componentToSize[component];
+        }
+
+        int area = 1;
+        unordered_set<int> seen;
 
         for (int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-
-            if (check(nx, ny, grid, visited)) {
-                ret += dfs(nx, ny, grid, visited, compId, curId);
+            int newX = x + dx[k];
+            int newY = y + dy[k];
+            if (isWithinGrid(newX, newY) &&
+                grid[newX][newY] == 1) {
+                int component = gridToComponent[newX][newY];
+                if (seen.find(component) == seen.end()) {
+                    area += componentToSize[component];
+                    seen.insert(component);
+                }
             }
         }
 
-        return ret;
+        return area;
     }
 
 public:
-    int largestIsland(vector<vector<int> >& grid) {
-        rows = grid.size();
-        cols = grid[0].size();
+    int largestIsland(vector<vector<int>>& grid) {
+        n = grid.size();
+        gridToComponent = vector<vector<int>>(n, vector<int>(n, 0));
 
-        int ans = 0;
-        int comp = 0;
-        vector<vector<bool> > visited(rows, vector<bool>(cols, false));
-        vector<vector<int> > compId(rows, vector<int>(cols));
-        unordered_map<int, int> idToSize;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) {
-                    int compSize = dfs(i, j, grid, visited, compId, comp);
-                    ans = max(ans, compSize);
-                    idToSize[comp] = compSize;
-                    comp++;
+        component = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && gridToComponent[i][j] == 0) {
+                    component++;
+                    int componentSize = 0;
+                    traverse(i, j, grid, componentSize);
+                    componentToSize[component] = componentSize;
                 }
             }
         }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 0) {
-                    unordered_map<int, int> tmap;
-                    for (int k = 0; k < 4; k++) {
-                        int x = i + dx[k];
-                        int y = j + dy[k];
-                        if (check(x, y, grid)) {
-                            tmap[compId[x][y]] = idToSize[compId[x][y]];
-                        }
-                    }
-
-                    int sum = 0;
-                    for (auto it : tmap) {
-                        sum += it.second;
-                    }
-
-                    ans = max(ans, sum + 1);
-                }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ans = max(ans, findBestAnswer(i, j, grid));
             }
         }
 
