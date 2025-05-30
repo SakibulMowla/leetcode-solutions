957a127b051221db5ee135c12367c6529f8cc6de
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-30 11:55:37 +0100

    Hard, Pattern, DP

--- solutions/0010. Regular Expression Matching.cpp
+++ solutions/0010. Regular Expression Matching.cpp
@@ -34,3 +34,47 @@ public:
         return solve(sLen - 1, pLen - 1, visited, s, p);
     }
 };
+
+// ------------------------------------------------------------------------
+
+class Solution {
+private:
+    int solve(string&s, string& p, int sIndex, int pIndex, vector<vector<int>>& dp) {
+        if (sIndex == s.size() && pIndex == p.size()) {
+            return 1;
+        }
+        if (pIndex == p.size()) {
+            return 0;
+        }
+        
+        if (sIndex == s.size()) {
+            while (pIndex + 1 < p.size() && p[pIndex + 1] == '*') {
+                pIndex += 2;
+            }
+            return pIndex == p.size();
+        }
+
+        int& ret = dp[sIndex][pIndex];
+        if (ret != -1) {
+            return ret;
+        }
+
+        bool match = (s[sIndex] == p[pIndex] || p[pIndex] == '.');
+        if (pIndex + 1 < p.size() && p[pIndex + 1] == '*') {
+            ret = solve(s, p, sIndex, pIndex + 2, dp) ||
+                  (match && solve(s, p, sIndex + 1, pIndex, dp));
+        } else {
+            ret = match && solve(s, p, sIndex + 1, pIndex + 1, dp);
+        }
+
+        return ret;
+    }
+
+public:
+    bool isMatch(string s, string p) {
+        int sLen = s.size(), pLen = p.size();
+        vector<vector<int>> dp(sLen, vector<int> (pLen, -1));
+
+        return bool(solve(s, p, 0, 0, dp));
+    }
+};
