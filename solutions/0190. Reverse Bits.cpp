a09bf4b6ff6d32275a1afdc2fc7b49eeaa0183c2
Sakibul Mowla <sakibulmowla@gmail.com>
2025-05-31 09:14:20 +0100

    Easy, Divide and Conquer, Bit Manipulation, Math

--- solutions/0190. Reverse Bits.cpp
+++ solutions/0190. Reverse Bits.cpp
@@ -25,3 +25,41 @@ public:
         return ans;
     }
 };
+
+// ---------------------------------------------
+
+// Memoization
+
+class Solution {
+private:
+    int byte_size = 8;
+    unordered_map<uint32_t, uint32_t> cache;
+
+    uint32_t reverseByte(uint32_t n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (cache.find(n) != cache.end()) {
+            return cache[n];
+        }
+
+        int ans = 0;
+        for (int i = 0; i < byte_size; i++) {
+            ans = (ans << 1) | (n & 1);
+            n >>= 1;
+        }
+
+        return cache[n] = ans;
+    }    
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int mask = (1 << byte_size) - 1;
+        int ans = 0;
+        for (int i = 0; i < 4; i++) {
+            ans = (ans << byte_size) | reverseByte(n & mask);
+            n >>= byte_size;
+        }
+
+        return ans;
+    }
+};
