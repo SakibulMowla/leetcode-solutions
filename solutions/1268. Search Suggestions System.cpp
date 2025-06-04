3943d93b9bbafa9e61c95092e9102c5b3f139fd5
Sakibul Mowla <sakibulmowla@gmail.com>
2025-06-04 23:18:38 +0100

    Medium, String, Trie, Binary Search

--- solutions/1268. Search Suggestions System.cpp
+++ solutions/1268. Search Suggestions System.cpp
@@ -1,3 +1,5 @@
+// Trie
+
 class TrieNode {
 public:
     bool isWord;
@@ -75,6 +77,74 @@ public:
         vector<vector<string>> ans(len);
 
         traverse(searchWord, ans);
+
+        return ans;
+    }
+};
+
+// ------------------------------------------------------------------
+
+// Binary Search
+
+class Solution {
+private:
+    bool containsFromBegin(string& word, string& prefix) {
+        if (word.size() < prefix.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < prefix.size(); i++) {
+            if (prefix[i] != word[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    int findIndex(vector<string>& words, string& prefix, int lo, int hi) {
+        int index = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (words[mid] < prefix) {
+                lo = mid + 1;
+            } else {
+                index = mid;
+                hi = mid - 1;
+            }
+        }
+
+        return index;
+    }
+
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        int n = products.size();
+        sort(products.begin(), products.end());
+
+        int len = searchWord.size();
+        vector<vector<string>> ans(len);
+
+        string prefix = "";
+        int lo = 0;
+        for (int i = 0; i < len; i++) {
+            prefix += searchWord[i];
+            int index = findIndex(products, prefix, lo, n - 1);
+
+            // binary search
+            if (index == -1) {
+                continue;
+            }
+
+            lo = index;
+
+            for (int k = 0; k < 3 && index + k < n; k++) {
+                string& word = products[index + k];
+                if (containsFromBegin(word, prefix)) {
+                    ans[i].push_back(word);
+                }
+            }
+        }      
 
         return ans;
     }
