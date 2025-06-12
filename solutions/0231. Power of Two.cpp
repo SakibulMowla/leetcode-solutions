976ab6d076ae04f6ba292bc16ee24a1d873d5567
Sakibul Mowla <sakibulmowla@gmail.com>
2025-06-12 09:28:00 +0100

    Easy, Math, Bit Manipulation

---
+++ solutions/0231. Power of Two.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        
+        return (n & (n - 1)) == 0;
+    }
+};
+
+// ----------------------------------------
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n < 0) {
+            return false;
+        }
+        
+        return __builtin_popcount(n) == 1;
+    }
+};
