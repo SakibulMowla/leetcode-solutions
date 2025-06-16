c7fa0740b8c1aee70c1d2364aa3ceef166aee4dc
Sakibul Mowla <sakibulmowla@gmail.com>
2025-06-16 10:38:02 +0100

    Medium, DP, Math, BST

--- solutions/0096. Unique Binary Search Trees.cpp
+++ solutions/0096. Unique Binary Search Trees.cpp
@@ -1,3 +1,34 @@
+// Recursive
+
+class Solution {
+private:
+    int solve(int n, vector<int>& dp) {
+        if (n <= 0)
+            return 1;
+
+        int ret = dp[n];
+        if (ret != -1)
+            return ret;
+
+        ret = 0;
+        for (int i = 1; i <= n; i++) {
+            ret += solve(i - 1, dp) * solve(n - i, dp);
+        }
+
+        return dp[n] = ret;
+    }
+
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
+    }
+};
+
+// ------------------------------------------------
+
+// Bottom-up
+
 class Solution {
 public:
     int numTrees(int n) {
